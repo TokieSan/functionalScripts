69a8ef7076ebfb09244a4c2bd3caa26bc61480df
Tokhy <elt0khy@protonmail.com>
2022-01-13 21:33:03 +0200

    fixed a bug in registeration script

--- registerInCoursesFast.sh
+++ registerInCoursesFast.sh
@@ -1,6 +1,9 @@
-declare -a arr=("13653" "13913" "14482" "14484" "13897" "13602" "13303" "13680")
+declare -a arr=("" "33575" "33576" "33574" "34474" "33683" "33283")
  
 for val in ${arr[@]}; do
 	xte "str $val"
 	xte "key Tab"
+	sleep .1
 done
+
+xte "key Return"
